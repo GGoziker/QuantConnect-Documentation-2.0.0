92c8a843a2565d1de998d43b6e5993cc319d172a
Gahl Goziker <gahl.goziker@gmail.com>
2020-07-29 12:16:47 -0700

    Fix project name

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ sys.path.append(os.path.abspath('sphinxext'))
 
 # -- Project information -----------------------------------------------------
 
-project = 'intersphinx-practice'
+project = 'Using-QC'
 copyright = '2020, gg'
 author = 'Gahl Goziker'
 master_doc = 'index'
